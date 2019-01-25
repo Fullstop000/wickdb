44e34320d117d95cc9f6d201840902ce9bb9d7c5
fullstop000 <fullstop1005@gmail.com>
2019-01-25 19:06:08 +0800

    initial commit
    Signed-off-by: fullstop000 <fullstop1005@gmail.com>

---
+++ src/db.rs
@@ -0,0 +1,36 @@
+// Copyright 2019 Fullstop000 <fullstop1005@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use util::error::WickError;
+use config::Config;
+
+pub struct DB {
+}
+
+pub fn open_db(config: Config) -> DB {
+
+}
+
+impl DB {
+    pub fn new() -> DB {
+
+    }
+    pub fn write(&self, key: Vec<&u8>) -> Result<(), WickError> {
+
+    }
+
+    pub fn get(&self, key: Vec<&u8>) -> Option<Vec<&u8>> {
+
+    }
+
+}
