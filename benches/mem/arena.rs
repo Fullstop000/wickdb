602953422beb5a3d7593f3e347d199561ecab1e3
Fullstop000 <fullstop1005@gmail.com>
2020-02-04 17:52:52 +0800

    update to 1.41.0
    Signed-off-by: Fullstop000 <fullstop1005@gmail.com>

--- benches/mem/arena.rs
+++ benches/mem/arena.rs
@@ -5,7 +5,7 @@ static CHUNK_SIZE: [usize; 6] = [256, 1024, 4096, 10000, 16384, 33333];
 
 fn bench_allocate(c: &mut Criterion) {
     let mut group = c.benchmark_group("BlockArena::allocate");
-    for size in CHUNK_SIZE.clone().into_iter() {
+    for size in CHUNK_SIZE.clone().iter() {
         group.bench_with_input(
             BenchmarkId::from_parameter(size),
             size,
@@ -22,7 +22,7 @@ fn bench_allocate(c: &mut Criterion) {
 
 fn bench_allocate_aligned(c: &mut Criterion) {
     let mut group = c.benchmark_group("BlockArena::allocate_aligned");
-    for size in CHUNK_SIZE.clone().into_iter() {
+    for size in CHUNK_SIZE.clone().iter() {
         group.bench_with_input(
             BenchmarkId::from_parameter(size),
             size,
