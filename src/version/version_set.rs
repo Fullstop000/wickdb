5a2f3d4d11256d805ee372963811f413a9bcda75
AliceLanniste <1399789151@qq.com>
2020-02-13 19:35:24 +0800

    cargo fmt again

--- src/version/version_set.rs
+++ src/version/version_set.rs
@@ -1011,12 +1011,8 @@ impl<S: Storage + Clone> DerivedIterFactory for FileIterFactory<S> {
         } else {
             let file_number = decode_fixed_64(value);
             let file_size = decode_fixed_64(&value[std::mem::size_of::<u64>()..]);
-            self.table_cache.new_iter(
-                self.icmp.clone(),
-                self.options,
-                file_number,
-                file_size,
-            )
+            self.table_cache
+                .new_iter(self.icmp.clone(), self.options, file_number, file_size)
         }
     }
 }
